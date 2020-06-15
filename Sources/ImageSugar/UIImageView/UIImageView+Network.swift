c6b90ef5e25ade10400a16c927b791e3aacbab02
eonist <30n1st@gmail.com>
2020-06-15 21:28:01 +0200

    Update UIImageView+Network.swift

--- Sources/ImageSugar/UIImageView/UIImageView+Network.swift
+++ Sources/ImageSugar/UIImageView/UIImageView+Network.swift
@@ -12,7 +12,7 @@ extension UIImageView {
    /**
     * Create UIIMageView from filePath
     */
-   public convenience init(filePath: String) {//should throw?
+   public convenience init(filePath: String) { // should throw?
       let img = UIImage(contentsOfFile: filePath)
       self.init(image: img)
    }
@@ -24,11 +24,11 @@ extension UIImageView {
       thread.async {
          UIImage.image(webPath: webPath) { (image: UIImage?, error: UIImage.IMGError?) in
             if let img = image {
-               DispatchQueue.main.async {//UI stuff must happen on the main thread
-                  self.image = img//set the image
+               DispatchQueue.main.async { // UI stuff must happen on the main thread
+                  self.image = img // set the image
                }
             } else {
-               onError(error)//call the error handler
+               onError(error) // call the error handler
             }
          }
       }
